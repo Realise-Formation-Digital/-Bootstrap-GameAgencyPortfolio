31b1a623de96c1f8b77d3fecbf19e0babb9405c7
zanotelli <zanotellisimon@gmail.com>
2020-12-22 20:49:03 +0100

    modification footer + lorem lipsum fichier csv messages

--- www/admin/index.php
+++ www/admin/index.php
@@ -101,6 +101,16 @@ if (($handle = fopen("../messages.csv", "r")) !== FALSE) {
     </div>
 
 </section>
+<footer>
+<div class="container">
+        <div class="row" style="height: 220px">
+            <div class="col-12">
+<?php
+echo "<p>Copyright &copy; " . date("Y") . " ADALT Agency</p>";
+?>          </div>
+      </div>
+</div>
+</footer>
 <script>
 jQuery(document).ready(function($){
   // Get current path and find target link
@@ -116,17 +126,5 @@ jQuery(document).ready(function($){
   target.addClass('active');
 });
 </script>
-<script src="script.js"></script>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
-        crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
-        integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s"
-        crossorigin="anonymous"></script>
-<script src="js/formValidation.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 </body>
 </html>
